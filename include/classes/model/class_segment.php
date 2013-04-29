88f7b24c8531dde0aac3598b9b31a4f738ecb807
Dactar <github@dactar.ch>
2013-04-30 00:12:33 +0200

    Added code for errors displaying

--- include/classes/model/class_segment.php
+++ include/classes/model/class_segment.php
@@ -77,50 +77,62 @@ class segment
 		$this->web_page->render();
 	    	return "On affiche les actions disponibles"; 
 	}
-
-	function insert_submit()
+	
+	function create_submit()
 	{
-		$_POST=utf8_array_decode($_POST);
-		$query="INSERT into segment (parent_id, appl_dict_id, code, description, rank_n, type_dict_id, prod_f, supported_f, default_f) VALUES (:PARENT_ID,:APPL_ID,:CODE,:DESCRIPTION,:RANK_N,:TYPE_ID,:PROD_F,:SUPPORTED_F,:DEFAULT_F)";
-                $row = $this->db->prepare($query);
-                if ( $_POST[parent_id] != "0" )
-                {
-                    $row->bindParam(':PARENT_ID',   $_POST[parent_id],   PDO::PARAM_INT);
-                }
-                $row->bindParam(':APPL_ID',     $_POST[appl_dict_id],     PDO::PARAM_INT);
-                $row->bindParam(':CODE',        $_POST[code],        PDO::PARAM_STR);
-                $row->bindParam(':DESCRIPTION', $_POST[description], PDO::PARAM_STR);
-                $row->bindParam(':RANK_N',      $_POST[rank_n],      PDO::PARAM_INT);
-                $row->bindParam(':TYPE_ID',     $_POST[type_dict_id],     PDO::PARAM_INT);
-                $row->bindParam(':PROD_F',      $_POST[prod_f],      PDO::PARAM_INT);
-                $row->bindParam(':SUPPORTED_F', $_POST[supported_f], PDO::PARAM_INT);
-                $row->bindParam(':DEFAULT_F',   $_POST[default_f], PDO::PARAM_INT);
-                $row->execute();
-                sqlerror($this->db,$query);
+		$errors=false;
+		if ($_POST[appl_dict_id] == ""){print "<div class='ERR'>Erreur : le segment n'a pas d'application</div>";$errors=true;}
+        	if ($_POST[type_dict_id] == ""){print "<div class='ERR'>Erreur : le segment n'a pas de type</div>";$errors=true;}
+
+		if (!$errors)
+		{
+			$_POST=utf8_array_decode($_POST);
+			$query="INSERT into segment (parent_id, appl_dict_id, code, description, rank_n, type_dict_id, prod_f, supported_f, default_f) VALUES (:PARENT_ID,:APPL_ID,:CODE,:DESCRIPTION,:RANK_N,:TYPE_ID,:PROD_F,:SUPPORTED_F,:DEFAULT_F)";
+                	$row = $this->db->prepare($query);
+                	if ( $_POST[parent_id] != "0" )
+                	{
+                    		$row->bindParam(':PARENT_ID',   $_POST[parent_id],   PDO::PARAM_INT);
+                	}
+                	$row->bindParam(':APPL_ID',     $_POST[appl_dict_id],     PDO::PARAM_INT);
+              		$row->bindParam(':CODE',        $_POST[code],        PDO::PARAM_STR);
+                	$row->bindParam(':DESCRIPTION', $_POST[description], PDO::PARAM_STR);
+                	$row->bindParam(':RANK_N',      $_POST[rank_n],      PDO::PARAM_INT);
+                	$row->bindParam(':TYPE_ID',     $_POST[type_dict_id],     PDO::PARAM_INT);
+                	$row->bindParam(':PROD_F',      $_POST[prod_f],      PDO::PARAM_INT);
+                	$row->bindParam(':SUPPORTED_F', $_POST[supported_f], PDO::PARAM_INT);
+                	$row->bindParam(':DEFAULT_F',   $_POST[default_f], PDO::PARAM_INT);
+                	$row->execute();
+                	sqlerror($this->db,$query);
+		}
 		return "On crée le segment";	
 	}
 	function update_submit()
 	{
-		$_POST=utf8_array_decode($_POST);	
-		$query="UPDATE segment set parent_id = :PARENT_ID, appl_dict_id = :APPL_ID, code = :CODE, description = :DESCRIPTION, rank_n = :RANK_N, type_dict_id = :TYPE_ID, prod_f = :PROD_F, supported_f = :SUPPORTED_F, default_f = :DEFAULT_F where id = :ID";
+		$errors=false;
+		if ($_POST[appl_dict_id] == ""){print "<div class='ERR'>Erreur : le segment n'a pas d'application</div>";$errors=true;}
+        	if ($_POST[type_dict_id] == ""){print "<div class='ERR'>Erreur : le segment n'a pas de type</div>";$errors=true;}
+		if (!$errors)
+		{	
+			$_POST=utf8_array_decode($_POST);	
+			$query="UPDATE segment set parent_id = :PARENT_ID, appl_dict_id = :APPL_ID, code = :CODE, description = :DESCRIPTION, rank_n = :RANK_N, type_dict_id = :TYPE_ID, prod_f = :PROD_F, supported_f = :SUPPORTED_F, default_f = :DEFAULT_F where id = :ID";
 
-                $row = $this->db->prepare($query);
-                $row->bindParam(':ID', $_POST[id], PDO::PARAM_INT);
-                if ( $_POST[parent_id] != "0" )
-                {
-                $row->bindParam(':PARENT_ID',   $_POST[parent_id],   PDO::PARAM_INT);
-                }
-                $row->bindParam(':APPL_ID',     $_POST[appl_dict_id],     PDO::PARAM_INT);
-                $row->bindParam(':CODE',        $_POST[code],        PDO::PARAM_STR);
-                $row->bindParam(':DESCRIPTION', $_POST[description], PDO::PARAM_STR);
-                $row->bindParam(':RANK_N',      $_POST[rank_n],      PDO::PARAM_INT);
-                $row->bindParam(':TYPE_ID',     $_POST[type_dict_id],     PDO::PARAM_INT);
-                $row->bindParam(':PROD_F',      $_POST[prod_f],      PDO::PARAM_INT);
-                $row->bindParam(':SUPPORTED_F', $_POST[supported_f], PDO::PARAM_INT);
-		$row->bindParam(':DEFAULT_F',   $_POST[default_f], PDO::PARAM_INT);
-                $row->execute();
-                sqlerror($this->db,$query);
-	
+			$row = $this->db->prepare($query);
+			$row->bindParam(':ID', $_POST[id], PDO::PARAM_INT);
+			if ( $_POST[parent_id] != "0" )
+                	{
+                		$row->bindParam(':PARENT_ID',   $_POST[parent_id],   PDO::PARAM_INT);
+                	}
+                	$row->bindParam(':APPL_ID',     $_POST[appl_dict_id],     PDO::PARAM_INT);
+                	$row->bindParam(':CODE',        $_POST[code],        PDO::PARAM_STR);
+                	$row->bindParam(':DESCRIPTION', $_POST[description], PDO::PARAM_STR);
+                	$row->bindParam(':RANK_N',      $_POST[rank_n],      PDO::PARAM_INT);
+			$row->bindParam(':TYPE_ID',     $_POST[type_dict_id],     PDO::PARAM_INT);
+                	$row->bindParam(':PROD_F',      $_POST[prod_f],      PDO::PARAM_INT);
+                	$row->bindParam(':SUPPORTED_F', $_POST[supported_f], PDO::PARAM_INT);
+			$row->bindParam(':DEFAULT_F',   $_POST[default_f], PDO::PARAM_INT);
+                	$row->execute();
+                	sqlerror($this->db,$query);
+		}	
 	    	return "On met à jour le segment";
 	}
 
